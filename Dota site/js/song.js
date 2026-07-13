670a7127ca0eb1d39cc8a6f5b1d692d148cfb20a
Lovelovep <139171936+Lovelovep@users.noreply.github.com>
2026-07-13 13:27:38 +0500

    Delete Dota site directory

--- Dota site/js/song.js
+++
@@ -1,7 +0,0 @@
-let slider = document.getElementById('mySlider');
-let volumeVideo = document.getElementById('videoVolume')
-
-slider.addEventListener('input', function() {
-  volumeVideo.volume = slider.value/100;
-  volumeVideo.style.filter = `brightness(${slider.value}%)`;
-});
